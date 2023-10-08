d0f65255d6a3e016debf9909152c70166acf76de
Nand Kishore Ola <nandkishoreola0786b@gmail.com>
2023-10-08 22:11:35 +0530

    controller for the user Login

--- controllers/auth.js
+++ controllers/auth.js
@@ -45,3 +45,36 @@ export const register = async (req, res)=>{
     }
 }
 
+
+export const login = async (req, res)=>{
+    try {
+        const {email, pass} = req.body;
+        if(!email || !pass){
+            return res.status(400).json({ "stat":"OK","error": "Missing data","Verified":false,"message":"Access Denied, Please send all data" });
+        }
+        const user = await userModel.findOne({email:email});
+        if(!user){
+            return res.status(400).json({ "stat":"OK","error": "user not exist","Verified":false,"message":"Access Denied, Please Login again" });
+        }
+        const ismatch = await bcrypt.compare(pass,user.pass);
+        if(!ismatch){
+            return res.status(400).json({"stat":"OK","Error":"","Verified":false,"message":"Access Denied, Please Login again"})
+        }
+        const currentDate = new Date();
+        const sevenDaysLater = new Date(currentDate.getTime() + 7 * 24 * 60 * 60 * 1000); // Calculate the date 7 days from now
+
+        const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+        expiresIn: '7d', // Specify the expiration in 7 days
+        });
+
+        res.cookie('jwt', token, {
+        expires: sevenDaysLater, // Set the cookie expiration time
+        httpOnly: true,
+        });
+
+        return res.status(201).json({"stat":"OK","Error":"","Verified":true,"message":"token generated","profile":{id:user._id, name:user.name, email:user.email, token:token}})
+    } catch (error) {
+        console.log(error.message)
+        return res.status(500).json({"stat":"OK","Error":error.message,"Verified":false,"message":"Internal Server Problem"})
+    }
+}
