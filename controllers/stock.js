f06d57a9425384dc13a161f26638c8d5d9088654
Nand Kishore Ola <nandkishoreola0786b@gmail.com>
2023-10-08 22:42:44 +0530

    controllers to update subscriber update

--- controllers/stock.js
+++ controllers/stock.js
@@ -81,3 +81,39 @@ export const stockList = async (req, res) => {
     });
   }
 };
+
+
+export const subscriberUpdate = async(req,res)=>{
+  try {
+      // update the Number of the suscriber, We should use lock mechanism here for the data safe and correct data
+      const stock = req.body.stock;
+      const newvalue = req.body.newSubscriber;
+      console.log(stock, newvalue);
+       // Find the existing subscriber by the identifier
+      const existingSubscriber = await StockListModel.findOne({ stock: stock });
+
+
+
+       // Update the subscriber's stock information
+       existingSubscriber.subscriber+=newvalue;
+
+       // Save the updated subscriber
+       const update=await existingSubscriber.save();
+
+       res.status(201).json({ 
+          stat:"OK",
+          error:'',
+          Verified:true,
+          message: 'Subscriber updated successfully',
+          stock:update 
+      });
+  } catch (error){
+      console.error(error);
+      return res.status(500).json({
+        stat:"OK",
+        error: error.message,
+        Verified:true,
+        message: 'An error occurred while retrieving stocks.',
+      });
+    }
+}
